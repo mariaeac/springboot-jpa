8d0e3236cd6d24e7893d17f7d472762fa6126429
Maria Eduarda <meac21kj@gmail.com>
2024-12-11 03:44:09 -0300

    Project created

---
+++ src/main/java/com/meac/cousespringboot/CouseSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.meac.cousespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouseSpringbootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CouseSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meac/cousespringboot/CouseSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meac.cousespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouseSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
